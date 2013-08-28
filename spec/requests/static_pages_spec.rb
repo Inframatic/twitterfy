9ae072b9db0694b7ce89ef27420aac0b44d4d9c5
inframatic <isteffy@hotmail.com>
2013-08-28 12:34:52 -0400

    adding the capybara DSL to the RSpec helper file

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
   describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
+
+    it "should have the content 'Twitterfy'" do
+    	visit '/static_pages/home'
+    	expect(page).to have_content('Twitterfy')
     end
   end
 end
