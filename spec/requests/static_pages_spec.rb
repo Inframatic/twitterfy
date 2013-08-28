f76f89c28b7dfb15309a28b049ed538c8f247f59
inframatic <isteffy@hotmail.com>
2013-08-28 12:46:13 -0400

    added static home, about and help pages and run passing tests for all three

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,12 +1,28 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
 
-  describe "GET /static_pages" do
+  describe "Home page" do
 
     it "should have the content 'Twitterfy'" do
     	visit '/static_pages/home'
     	expect(page).to have_content('Twitterfy')
+    end
+  end
+
+  describe "Help page" do
+
+  	it "should have the content 'Help'" do
+  		visit '/static_pages/help'
+  		expect(page).to have_content('Help')
+  	end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
     end
   end
 end
