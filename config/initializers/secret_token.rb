4dda395dfcfefb744f054318e8c6bc74359311b6
inframatic <isteffy@hotmail.com>
2013-08-28 11:11:10 -0400

    defined a secure token and required 'securerandom' in the secret_token.rb file in initializers

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,18 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+	token_file = Rails.root.join('.secret')
+	if File.exist?(token_file)
+		#Use the existing token
+    File.read(token_file).chomp
+  else
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
 Twitterfy::Application.config.secret_key_base = '33def802cbf260cfa89164a1ea83c2ee38ec68f27c791dd38d33e56acc3d4e79f34253a0942affb167101f141ef00dfb2b0b40b910f2e7deac8cf7a3ce8303df'
