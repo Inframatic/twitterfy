7cb4039b0ca82e0d8195356bd076e18c528d933e
inframatic <isteffy@hotmail.com>
2013-09-01 12:22:43 -0400

    Added def index to view all users. working on view-index.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.all
+  end
 
   def new
   	@user = User.new
@@ -41,12 +45,12 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-    
-
      # Before filters
-
     def signed_in_user
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in." unless
+      end
     end
 
     def correct_user
