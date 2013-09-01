08e7818eb5bb002a65c81c5a72249a1816779e87
inframatic <isteffy@hotmail.com>
2013-09-01 12:11:39 -0400

    updated def edit in users controller and added def update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,21 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   def show
   	@user = User.find(params[:id])
   end
